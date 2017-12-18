c4d0b74c29de72e3b04b9051122d68a46c4caa2a
PAVE <tanguy.pave@capgemini.com>
2017-12-18 15:13:19 +0100

    googleMap

--- src/pages/googlemap/googlemap.ts
+++ src/pages/googlemap/googlemap.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, Platform } from 'ionic-angular';
-import { GoogleMaps, GoogleMap, GoogleMapsEvent,
-  VisibleRegion, ILatLng
-} from '@ionic-native/google-maps';
+import { IonicPage } from 'ionic-angular';
+import { GoogleMaps, GoogleMap, GoogleMapsEvent, GoogleMapOptions } from '@ionic-native/google-maps';
 
 /**
  * Generated class for the GooglemapPage page.
@@ -20,48 +18,71 @@ export class GooglemapPage {
 
     map: GoogleMap;
   
-    constructor(
-      public navCtrl: NavController, private platform: Platform) {  // <-- no longer need to define in constructor
+    constructor() {  // <-- no longer need to define in constructor
       
-      this.platform.ready().then(() => this.loadMaps());
     }
   
     ionViewLoaded() {
-      console.log("gne")
-      // this.loadMap();
-    }
-
-    loadMaps() {
+      console.log("Génération de la map..");
       this.createMap();
     }
 
-
     createMap() {
-      this.map = GoogleMaps.create('map_canvas');
+      let mapOptions: GoogleMapOptions = {
+        camera: {
+          target: {
   
-      this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
-        console.log("map is ready to use.");
-  
-        this.createMarkers();
+            lat: 43.3168,
+            lng: 5.36
+          },
+          zoom: 18,
+          tilt: 30
+        }
+      };
+
+      let elements: HTMLElement = document.getElementById('map');
+      this.map = GoogleMaps.create(elements, mapOptions);
+
+      this.map.one(GoogleMapsEvent.MAP_READY)
+      .then(() => {
+        console.log('Map is ready!');
+        this.createMarker();
       });
+
     }
 
-    createMarkers() {
-      let latLngBounds: VisibleRegion = this.map.getVisibleRegion();
-      let sw: ILatLng = latLngBounds.southwest;
-      let ne: ILatLng = latLngBounds.northeast;
-      let diffY: number = (ne.lat - sw.lat);
-      let diffX: number = (ne.lng - sw.lng);
+    createMarker() {
+      this.map.addMarker({
+        title: 'Ionic',
+        icon: 'blue',
+        animation: 'DROP',
+        position: {
+          lat: 43.0741904,
+          lng: -89.3809802
+        }
+      })
+      .then(marker => {
+        marker.on(GoogleMapsEvent.MARKER_CLICK)
+          .subscribe(() => {
+            alert('clicked');
+          });
+      });
+      // let latLngBounds: VisibleRegion = this.map.getVisibleRegion();
+      // let sw: ILatLng = latLngBounds.southwest;
+      // let ne: ILatLng = latLngBounds.northeast;
+      // let diffY: number = (ne.lat - sw.lat);
+      // let diffX: number = (ne.lng - sw.lng);
   
-      for (let i = 0; i < 100; i++) {
-        this.map.addMarker({
-          'position': {
-            'lat': sw.lat + diffY * Math.random(),
-            'lng': sw.lng  + diffX * Math.random()
-          }
-        });
-      }
+      // for (let i = 0; i < 100; i++) {
+      //   this.map.addMarker({
+      //     'position': {
+      //       'lat': sw.lat + diffY * Math.random(),
+      //       'lng': sw.lng  + diffX * Math.random()
+      //     }
+      //   });
+      // }
     }
+    
 
   
     // loadMap() {
